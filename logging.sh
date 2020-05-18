37d807057490b104e22dfab151464ca341ef61b4
sshrestha <sujankshrestha@gmail.com>
2020-05-19 09:52:07 +1000

    updated config to handle time

--- logging.sh
+++ logging.sh
@@ -1,6 +1,6 @@
 i=0
 while true; 
-    do echo "{\"app_name\": \"date-logger\", \"value\": $i, \"date\": \"$(date)\"}" >> /tmp/log/app.log; 
+    do echo "{\"app_name\": \"date-logger\", \"value\": $i, \"time\": \"$(date)\"}" >> /tmp/log/app.log;
     i=$((i+1)); 
-    sleep 3; 
+    sleep 3;
 done
